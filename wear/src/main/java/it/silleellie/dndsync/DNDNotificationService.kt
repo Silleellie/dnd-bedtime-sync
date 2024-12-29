3f060ed80149d1235e48e46e7b649910f787c977
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-12-29 13:07:11 -0500

    refactor: migrate wear dnd listeners to Kotlin

--- wear/src/main/java/it/silleellie/dndsync/DNDNotificationService.kt
+++ wear/src/main/java/it/silleellie/dndsync/DNDNotificationService.kt
@@ -1,34 +1,22 @@
-package it.silleellie.dndsync;
+package it.silleellie.dndsync
+
+import android.service.notification.NotificationListenerService
+import android.util.Log
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Tasks
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.CapabilityInfo
+import com.google.android.gms.wearable.Wearable
+import it.silleellie.dndsync.shared.WearSignal
+import org.apache.commons.lang3.SerializationUtils
+import java.util.concurrent.ExecutionException
 
 
-import android.service.notification.NotificationListenerService;
-import android.util.Log;
-
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.Tasks;
-import com.google.android.gms.wearable.CapabilityClient;
-import com.google.android.gms.wearable.CapabilityInfo;
-import com.google.android.gms.wearable.Node;
-import com.google.android.gms.wearable.Wearable;
-
-import org.apache.commons.lang3.SerializationUtils;
-
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-
-import it.silleellie.dndsync.shared.WearSignal;
-
-public class DNDNotificationService extends NotificationListenerService {
-    private static final String TAG = "DNDNotificationService";
-    private static final String DND_SYNC_CAPABILITY_NAME = "dnd_sync";
-    private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
-
-    public static boolean running = false;
-
-    @Override
-    public void onListenerConnected() {
-        Log.d(TAG, "listener connected");
-        running = true;
+class DNDNotificationService : NotificationListenerService() {
+    override fun onListenerConnected() {
+        Log.d(TAG, "listener connected")
+        running = true
 
         //TODO enable/disable service based on app setting to save battery
 //        // We don't want to run a background service so disable and stop it
@@ -42,69 +30,87 @@ public class DNDNotificationService extends NotificationListenerService {
 //            Log.e(TAG, "Failed to stop service");
 //        }
     }
-//    private void disableServiceComponent() {
-//        PackageManager p = getPackageManager();
-//        ComponentName componentName = new ComponentName(this, DNDNotificationService.class);
-//        p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
-//    }
 
-    @Override
-    public void onListenerDisconnected() {
-        Log.d(TAG, "listener disconnected");
-        running = false;
+    //    private void disableServiceComponent() {
+    //        PackageManager p = getPackageManager();
+    //        ComponentName componentName = new ComponentName(this, DNDNotificationService.class);
+    //        p.setComponentEnabledSetting(componentName,PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+    //    }
+    override fun onListenerDisconnected() {
+        Log.d(TAG, "listener disconnected")
+        running = false
     }
 
 
-    @Override
-    public void onInterruptionFilterChanged (int interruptionFilter) {
-        Log.d(TAG, "interruption filter changed to " + interruptionFilter);
+    override fun onInterruptionFilterChanged(interruptionFilter: Int) {
+        Log.d(TAG, "interruption filter changed to $interruptionFilter")
 
         // preferences are now stored on the mobile app, so we send the signal nonetheless
         // and if the user ticked the relative option then it is synced to the phone
-        new Thread(() -> sendDNDSync(new WearSignal(interruptionFilter))).start();
+        Thread(Runnable { sendDNDSync(WearSignal(interruptionFilter)) }).start()
     }
 
-    private void sendDNDSync(WearSignal wearSignal) {
+    private fun sendDNDSync(wearSignal: WearSignal?) {
         // https://developer.android.com/training/wearables/data/messages
 
         // search nodes for sync
-        CapabilityInfo capabilityInfo = null;
+
+        var capabilityInfo: CapabilityInfo? = null
         try {
-            capabilityInfo = Tasks.await(
-                    Wearable.getCapabilityClient(this).getCapability(
-                            DND_SYNC_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-            Log.e(TAG, "execution error while searching nodes", e);
-            return;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            Log.e(TAG, "interruption error while searching nodes", e);
-            return;
+            capabilityInfo = Tasks.await<CapabilityInfo?>(
+                Wearable.getCapabilityClient(this).getCapability(
+                    DND_SYNC_CAPABILITY_NAME, CapabilityClient.FILTER_REACHABLE
+                )
+            )
+        } catch (e: ExecutionException) {
+            e.printStackTrace()
+            Log.e(TAG, "execution error while searching nodes", e)
+            return
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+            Log.e(TAG, "interruption error while searching nodes", e)
+            return
         }
 
         // send request to all reachable nodes
         // capabilityInfo has the reachable nodes with the dnd sync capability
-        Set<Node> connectedNodes = capabilityInfo.getNodes();
+        val connectedNodes = capabilityInfo.nodes
         if (connectedNodes.isEmpty()) {
             // Unable to retrieve node with transcription capability
-            Log.d(TAG, "Unable to retrieve node with sync capability!");
+            Log.d(TAG, "Unable to retrieve node with sync capability!")
         } else {
-            for (Node node : connectedNodes) {
+            for (node in connectedNodes) {
                 if (node.isNearby()) {
-                    byte[] data = SerializationUtils.serialize(wearSignal);
-                    Task<Integer> sendTask =
-                            Wearable.getMessageClient(this).sendMessage(node.getId(), DND_SYNC_MESSAGE_PATH, data);
+                    val data = SerializationUtils.serialize(wearSignal)
+                    val sendTask =
+                        Wearable.getMessageClient(this)
+                            .sendMessage(node.getId(), DND_SYNC_MESSAGE_PATH, data)
 
-                    sendTask.addOnSuccessListener(integer ->
-                            Log.d(TAG, "send successful! Receiver node id: " + node.getId())
-                    );
+                    sendTask.addOnSuccessListener(OnSuccessListener { integer: Int? ->
+                        Log.d(
+                            TAG,
+                            "send successful! Receiver node id: " + node.getId()
+                        )
+                    }
+                    )
 
-                    sendTask.addOnFailureListener(e ->
-                            Log.d(TAG, "send failed! Receiver node id: " + node.getId())
-                    );
+                    sendTask.addOnFailureListener(OnFailureListener { e: Exception? ->
+                        Log.d(
+                            TAG,
+                            "send failed! Receiver node id: " + node.getId()
+                        )
+                    }
+                    )
                 }
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "DNDNotificationService"
+        private const val DND_SYNC_CAPABILITY_NAME = "dnd_sync"
+        private const val DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync"
+
+        var running: Boolean = false
     }
 }
--- wear/src/main/java/it/silleellie/dndsync/DNDSyncListenerService.kt
+++ wear/src/main/java/it/silleellie/dndsync/DNDSyncListenerService.kt
@@ -1,43 +1,38 @@
-package it.silleellie.dndsync;
+package it.silleellie.dndsync
 
-import android.app.NotificationManager;
-import android.content.Context;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.provider.Settings;
-import android.util.Log;
-import androidx.annotation.NonNull;
-import com.google.android.gms.wearable.MessageEvent;
-import com.google.android.gms.wearable.WearableListenerService;
+import android.app.NotificationManager
+import android.os.Build
+import android.os.CombinedVibration
+import android.os.PowerManager
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.provider.Settings
+import android.util.Log
+import androidx.core.content.getSystemService
+import com.google.android.gms.wearable.MessageEvent
+import com.google.android.gms.wearable.WearableListenerService
+import it.silleellie.dndsync.shared.PhoneSignal
+import org.apache.commons.lang3.SerializationUtils
 
-import org.apache.commons.lang3.SerializationUtils;
-
-import it.silleellie.dndsync.shared.PhoneSignal;
-
-public class DNDSyncListenerService extends WearableListenerService {
-    private static final String TAG = "DNDSyncListenerService";
-    private static final String DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync";
-
-    @Override
-    public void onMessageReceived (@NonNull MessageEvent messageEvent) {
-
-        if (messageEvent.getPath().equalsIgnoreCase(DND_SYNC_MESSAGE_PATH)) {
-
-            Log.d(TAG, "received path: " + DND_SYNC_MESSAGE_PATH);
+class DNDSyncListenerService : WearableListenerService() {
+    override fun onMessageReceived(messageEvent: MessageEvent) {
+        if (messageEvent.getPath().equals(DND_SYNC_MESSAGE_PATH, ignoreCase = true)) {
+            Log.d(TAG, "received path: " + DND_SYNC_MESSAGE_PATH)
 
             // data is now a PhoneSignal object, it must be deserialized
-            byte[] data = messageEvent.getData();
-            PhoneSignal phoneSignal = SerializationUtils.deserialize(data);
+            val data = messageEvent.getData()
+            val phoneSignal = SerializationUtils.deserialize<PhoneSignal>(data)
 
-            Log.d(TAG, "dndStatePhone: " + phoneSignal.dndState);
+            Log.d(TAG, "dndStatePhone: " + phoneSignal.dndState)
 
             // get dnd state
-            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            int currentDndState = mNotificationManager.getCurrentInterruptionFilter();
+            val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val currentDndState = mNotificationManager.getCurrentInterruptionFilter()
 
-            Log.d(TAG, "currentDndState: " + currentDndState);
+            Log.d(TAG, "currentDndState: " + currentDndState)
             if (currentDndState < 0 || currentDndState > 4) {
-                Log.d(TAG, "Current DND state it's weird, should be in range [0,4]");
+                Log.d(TAG, "Current DND state is suspicious, should be in range [0,4]")
             }
 
             if (phoneSignal.dndState != null && phoneSignal.dndState == currentDndState) {
@@ -45,118 +40,129 @@ public class DNDSyncListenerService extends WearableListenerService {
                 // if dnd_as_bedtime and watch_sync_dnd are activated, when dnd is activated
                 // from the watch, dnd is activated to the phone and then bedtime is activated
                 // back on the watch. This early return avoids that.
-                return;
+                return
             } else if (phoneSignal.dndState != null) {
+                Log.d(
+                    TAG,
+                    "dndStatePhone != currentDndState: " + phoneSignal.dndState + " != " + currentDndState
+                )
 
-                Log.d(TAG, "dndStatePhone != currentDndState: " + phoneSignal.dndState + " != " + currentDndState);
+                changeDndSetting(mNotificationManager, phoneSignal.dndState)
 
-                changeDndSetting(mNotificationManager, phoneSignal.dndState);
-
-                Log.d(TAG, "vibrate: " + phoneSignal.vibratePref);
+                Log.d(TAG, "vibrate: " + phoneSignal.vibratePref)
                 if (phoneSignal.vibratePref) {
-                    vibrate();
+                    vibrate()
                 }
-
             }
 
-            String settingBedtimeStr = "setting_bedtime_mode_running_state";
-            int currentBedtimeState = Settings.Global.getInt(
-                    getApplicationContext().getContentResolver(), settingBedtimeStr, -1);
-
-            if (currentBedtimeState != -1) {
-                Log.d(TAG, "watch is the galaxy watch");
-            } else {
-                Log.d(TAG, "watch is not the galaxy watch");
-
-                settingBedtimeStr = "bedtime_mode";
-                currentBedtimeState = Settings.Global.getInt(
-                        getApplicationContext().getContentResolver(), settingBedtimeStr, -1);
-            }
-
-            Log.d(TAG, "currentBedtimeState: " + currentBedtimeState);
+            val currentBedtimeState = Settings.Global.getInt(
+                applicationContext.contentResolver, getBedtimeSettingName(), -1
+            )
 
             if (phoneSignal.bedtimeState != null && phoneSignal.bedtimeState != currentBedtimeState) {
-
-                Log.d(TAG, "bedtimeStatePhone != currentBedtimeState: " + phoneSignal.bedtimeState + " != " + currentBedtimeState);
+                Log.d(
+                    TAG,
+                    "bedtimeStatePhone != currentBedtimeState: " + phoneSignal.bedtimeState + " != " + currentBedtimeState
+                )
 
                 // activating/disabling bedtime also activates/disables dnd, just like
                 // when activating bedtime manually from the watch.
                 // dndState = 2 means it's activated, dndState = 1 means it's disabled
-                int dndState = phoneSignal.bedtimeState == 1 ? 2 : 1;
-                changeDndSetting(mNotificationManager, dndState);
+                val dndState = if (phoneSignal.bedtimeState == 1) 2 else 1
+                changeDndSetting(mNotificationManager, dndState)
 
-                boolean bedtimeModeSuccess = changeBedtimeSetting(settingBedtimeStr, phoneSignal.bedtimeState);
+                val bedtimeModeSuccess = changeBedtimeSetting(phoneSignal.bedtimeState)
                 if (bedtimeModeSuccess) {
-                    Log.d(TAG, "Bedtime mode value toggled");
+                    Log.d(TAG, "Bedtime mode value toggled")
                 } else {
-                    Log.d(TAG, "Bedtime mode toggle failed");
+                    Log.d(TAG, "Bedtime mode toggle failed")
                 }
 
-                if(phoneSignal.powersavePref) {
-
-                    boolean powerModeSuccess = changePowerModeSetting(phoneSignal.bedtimeState);
-                    if(powerModeSuccess) {
-                        Log.d(TAG, "Power Saver mode toggled");
+                if (phoneSignal.powersavePref) {
+                    val powerModeSuccess = changePowerModeSetting(phoneSignal.bedtimeState)
+                    if (powerModeSuccess) {
+                        Log.d(TAG, "Power Saver mode toggled")
                     } else {
-                        Log.d(TAG, "Power Saver mode toggle failed");
+                        Log.d(TAG, "Power Saver mode toggle failed")
                     }
                 }
 
-                Log.d(TAG, "vibrate: " + phoneSignal.vibratePref);
+                Log.d(TAG, "vibrate: " + phoneSignal.vibratePref)
                 if (phoneSignal.vibratePref) {
-                    vibrate();
+                    vibrate()
                 }
             }
-
         } else {
-            super.onMessageReceived(messageEvent);
+            super.onMessageReceived(messageEvent)
         }
     }
 
-    private void changeDndSetting(NotificationManager mNotificationManager, int newSetting) {
-
+    private fun changeDndSetting(mNotificationManager: NotificationManager, newSetting: Int) {
         if (mNotificationManager.isNotificationPolicyAccessGranted()) {
-            mNotificationManager.setInterruptionFilter(newSetting);
-            Log.d(TAG, "DND set to " + newSetting);
+            mNotificationManager.setInterruptionFilter(newSetting)
+            Log.d(TAG, "DND set to " + newSetting)
         } else {
-            Log.d(TAG, "attempting to set DND but access not granted");
+            Log.d(TAG, "attempting to set DND but access not granted")
         }
-
     }
 
-    private boolean changeBedtimeSetting(String settingBedtimeStr, int newSetting) {
-
-        boolean bedtimeModeSuccess = Settings.Global.putInt(
-                getApplicationContext().getContentResolver(), settingBedtimeStr, newSetting);
-        boolean zenModeSuccess = Settings.Global.putInt(
-                getApplicationContext().getContentResolver(), "zen_mode", newSetting);
-
-        return bedtimeModeSuccess && zenModeSuccess;
+    private fun getBedtimeSettingName(): String {
+        return if (Build.MANUFACTURER == "samsung") "setting_bedtime_mode_running_state" else "bedtime_mode"
     }
 
-    private boolean changePowerModeSetting(int newSetting) {
+    private fun changeBedtimeSetting(newSetting: Int): Boolean {
+        val settingBedtimeStr = getBedtimeSettingName()
+        val bedtimeModeSuccess = Settings.Global.putInt(
+            applicationContext.contentResolver, settingBedtimeStr, newSetting
+        )
+        val zenModeSuccess = Settings.Global.putInt(
+            applicationContext.contentResolver, "zen_mode", newSetting
+        )
 
-        boolean lowPower = Settings.Global.putInt(
-                getApplicationContext().getContentResolver(), "low_power", newSetting);
-        boolean restrictedDevicePerformance = Settings.Global.putInt(
-                getApplicationContext().getContentResolver(), "restricted_device_performance", newSetting);
+        return bedtimeModeSuccess && zenModeSuccess
+    }
 
-        boolean lowPowerBackDataOff = Settings.Global.putInt(
-                getApplicationContext().getContentResolver(), "low_power_back_data_off", newSetting);
-        boolean smConnectivityDisable = Settings.Secure.putInt(
-                getApplicationContext().getContentResolver(), "sm_connectivity_disable", newSetting);
+    /**
+     * Changes the power mode setting.
+     *
+     * **NOTE:** does not seem to work on non-samsung watches, like the Pixel Watch.
+     */
+    private fun changePowerModeSetting(newSetting: Int): Boolean {
+        val lowPower = Settings.Global.putInt(
+            getApplicationContext().getContentResolver(), "low_power", newSetting
+        )
+        val restrictedDevicePerformance = Settings.Global.putInt(
+            getApplicationContext().getContentResolver(),
+            "restricted_device_performance",
+            newSetting
+        )
+
+        val lowPowerBackDataOff = Settings.Global.putInt(
+            getApplicationContext().getContentResolver(), "low_power_back_data_off", newSetting
+        )
+        val smConnectivityDisable = Settings.Secure.putInt(
+            getApplicationContext().getContentResolver(), "sm_connectivity_disable", newSetting
+        )
 
         // screen timeout should be set to 10000 also, and ambient_tilt_to_wake should be set to 0
         // but previous variable states in those 2 cases must be stored and they do not seem to stick
         // and they are not so much important tbh (ambient tilt to wake is disabled anyways)
-
         return lowPower && restrictedDevicePerformance
-                && lowPowerBackDataOff && smConnectivityDisable;
+                && lowPowerBackDataOff && smConnectivityDisable
     }
 
-    private void vibrate() {
-        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(VibrationEffect.createOneShot(20, VibrationEffect.DEFAULT_AMPLITUDE));
+    private fun vibrate() {
+        val vibrator = getSystemService<Vibrator>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            vibrator?.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK))
+        } else {
+            @Suppress("DEPRECATION")
+            vibrator?.vibrate(500)
+        }
     }
 
+    companion object {
+        private const val TAG = "DNDSyncListenerService"
+        private const val DND_SYNC_MESSAGE_PATH = "/wear-dnd-sync"
+    }
 }
